2c9ca271b63509e6fdcaa788c917388e8949540a
John Stegeman <john.stegeman@gmail.com>
2024-08-10 11:45:42 -0400

    Replaced astrocommunity.debugging.nvim-bqf with astrocommunity.quickfix.nvim-bqf

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.flash-nvim", event = "VeryLazy" },
-  { import = "astrocommunity.debugging.nvim-bqf", event = "VeryLazy" },
+  { import = "astrocommunity.quickfix.nvim-bqf", event = "VeryLazy" },
   -- import/override with your plugins folder
 }
